08ba20339792746f933fbefb5ecb6691f5e963ae
marielyn52 <marielyn.otero@gmail.com>
2019-11-22 00:04:31 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,32 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#btn").addEventListener(click), () => {
+  document.querySelector("#random-excuses").innerHTML = generateExcuse();
+  };
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The elephant", "My abuelo", "Our tiger", "My sister"];
+  let what = [
+    "ate my pizza",
+    "peed my homework",
+    "bit my hand",
+    "broke my nails"
+  ];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex] + " " + ;
 };
